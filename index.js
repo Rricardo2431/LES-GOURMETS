f412371ede6431a18b9c82984ce4d184a0cbc8df
Brivael LE_ROUX <brivael.le.roux@gmail.com>
2023-04-19 11:33:58 +0200

    Add of the overlay function

---
+++ index.js
@@ -0,0 +1,12 @@
+// const search = document.querySelector("#searchText");
+// let couleur = "#fd1d1d";
+
+// function color(){
+//  search.style.animation = "moveSearchBar 4s 1 normal";
+// }
+
+
+const searchBtn = document.querySelector('#searchButton');
+searchBtn.addEventListener('click', function display(){
+
+})
